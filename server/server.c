c30745bf05fb87b76f21af8145322efa52ff95ac
MrTrojan52 <kirill_131097@mail.ru>
2019-03-10 02:50:09 +0300

    fix

--- server/server.c
+++ server/server.c
@@ -6,10 +6,10 @@ TCP_Status SERVER_STATUS = CLOSED;
 
 u_int32_t ACK_NUM = 0;
 u_int32_t SEQ_NUM = 1;
-
+int sd;
 void start_server(const char* port)
 {
-    int sd;
+
 
     char recv_buffer[PCKT_LEN];
 
@@ -70,6 +70,7 @@ void start_server(const char* port)
     wait_tcp_packet_with_flag(sd, recv_buffer, PCKT_LEN, 0, TCPFlag_SYN, htons(atoi(port)));
     set_status(&SERVER_STATUS, SYN_RECEIVED);
     printf("\nSYN: Client request connection\n");
+    ACK_NUM = tcp->acknowledgeNumber + 1;
     accept_client(ip->destinationIP, htons(atoi(port)), ip->sourceIP, tcp->sourcePort);
 
 }
@@ -82,17 +83,27 @@ int accept_client(in_addr_t dip, u_int16_t dport, in_addr_t sip, u_int16_t sport
 
     struct ipheader *ip = (struct ipheader *) buf;
     int csock = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (csock < 0)
+    {
+        perror("socket() error");
+
+        exit(-1);
+    }
     struct sockaddr_in din;
 
     din.sin_family = AF_INET;
     din.sin_port = sport;
     din.sin_addr.s_addr = sip;
 
-    bind(csock, (struct sockaddr*)&din, sizeof(din));
+    if(bind(csock, (struct sockaddr*)&din, sizeof(din)) < 0)
+    {
+        perror("bind() error");
 
-    build_packet(buf, dip, dport, sip, sport, TCPFlag_SYN | TCPFlag_ACK, NULL, 0, SEQ_NUM, ++ACK_NUM);
+        exit(-1);
+    }
 
-    if(sendto(csock, buf, ip->totalLength, 0, (struct sockaddr *)&din, sizeof(din)) < 0)
+    build_packet(buf, dip, dport, sip, sport, TCPFlag_SYN | TCPFlag_ACK, NULL, 0, SEQ_NUM, ACK_NUM);
+    if(sendto(sd, buf, ip->totalLength, 0, (struct sockaddr *)&din, sizeof(din)) < 0)
     {
         perror("sendto() error");
 
